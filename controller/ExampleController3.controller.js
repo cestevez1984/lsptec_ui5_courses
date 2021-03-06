5fc941a1e608c78f58b2bfe24b1376ed8ac0664f
cestevez1984 <ingcarlosestevez@gmail.com>
2021-03-05 19:27:31 -0800

    Upload project

---
+++ controller/ExampleController3.controller.js
@@ -0,0 +1,13 @@
+sap.ui.define(["sap/ui/core/mvc/Controller"],
+    function(Controller){
+            return Controller.extend("root.controller.ExampleController3",{
+                    "onInit":function(){
+
+                    },
+                    "confirm":function(){
+
+                    }
+
+            });
+    }
+);
